26b1c09164d3db0497523d65cfdbd319abafb9f2
Saad <mohdsaad931@gmail.com>
2023-02-27 23:24:47 +0530

    Initial commit

---
+++ dist/script.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+// Connect to the PgAdmin database
+$conn = pg_connect("host=localhost dbname=mydatabase user=mydatabaseuser password=mypassword");
+
+// Fetch the data from the table
+$result = pg_query($conn, "SELECT * FROM mytable");
+$data = pg_fetch_all($result);
+
+// Perform any necessary conversions
+// ...
+
+// Return the data as JSON
+header('Content-Type: application/json');
+echo json_encode($data);
+*/
+?>
